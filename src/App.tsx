15bf6d033498df01476d46613bab74dbf3badb56
Valerka.ya <lera.chazova2012@yandex.ru>
2025-09-19 12:49:29 +0300

    configurate aliases, router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <>
-      <div>PM Client Interafce</div>
-    </>
-  );
+import { Routes, Route, Outlet } from "react-router-dom";
+import AuthPage from "@/pages/AuthPage";
+import ProjectsPage from "@/pages/ProjectsPage";
+import ProjectDetailsPage from "@/pages/ProjectsPage/ProjectDetailsPage";
+import ProjectTasksPage from "@/pages/ProjectsPage/ProjectTasksPage";
+import HomePage from "@/pages/HomePage";
+
+function ProjectsLayout() {
+  return <Outlet />; // место для вложенных страниц
 }
 
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/projects/:projectId" element={<ProjectsLayout />}>
+        <Route index element={<ProjectDetailsPage />} /> {/* /projects/:id */}
+        <Route path="tasks" element={<ProjectTasksPage />} />{" "}
+        {/* /projects/:id/tasks */}
+      </Route>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+}
